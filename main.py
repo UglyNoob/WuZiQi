e69534dd2ad4ba513367c871f08894113fe97290
UglyNoob <1253328221@qq.com>
2019-05-05 10:25:26 +0000

    First

--- main.py
+++ main.py
@@ -2,3 +2,15 @@
 import pygame,sys
 pygame.init()
 screen=pygame.display.set_mode((601,901))
+
+play_map=[]
+i,j=0,0
+while i<=30:
+    line=[]
+    while j<=30:
+        line.append(' ')
+        j+=1
+    i+=1
+del i,j,line
+player='b'
+
